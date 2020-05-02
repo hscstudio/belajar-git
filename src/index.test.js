97ff6870949df64d6d2468ccd3dc62a726ea2dfb
Hafid Mukhlasin <hafidmukhlasin@gmail.com>
2020-05-03 03:37:55 +0700

    update fitur contributor

--- src/index.test.js
+++ src/index.test.js
@@ -2,5 +2,6 @@ const found = require('./index')
 
 test('contributor ready', () => {
   expect(found('Hafid Mukhlasin')).toBe(true)
+  expect(found('Kungfu Koding')).toBe(true)
   // expect(found('Namamu')).toBe(true)
 })
