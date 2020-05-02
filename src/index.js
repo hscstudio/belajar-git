cad298424230c821e1d76860466fe9295500925a
Hafid Mukhlasin <hafidmukhlasin@gmail.com>
2020-05-03 03:49:23 +0700

    update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const contributors = [
   { name: 'Hafid Mukhlasin', link: 'https://github.com/hscstudio' },
   { name: 'Karnaval', link: 'https://github.com/karnaval' },
   { name: 'Kungfu Koding', link: 'https://github.com/kungfukoding' },
+  
   // { name: 'namamu', link: 'link_githubmu' },
 ]
 
