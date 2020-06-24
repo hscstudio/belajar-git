cb8bf8570bf2db24f385c09ad0307f481f2ea618
sigdewa <sigdewa>
2020-06-24 08:05:19 +0700

    update fitur contributor

--- src/index.test.js
+++ src/index.test.js
@@ -6,5 +6,6 @@ test('contributor ready', () => {
   expect(found('easyphpframework')).toBe(true)
   expect(found('Tio Andri Prasetyo')).toBe(true)
   expect(found('Tesmo')).toBe(true)
+  expect(found('Sigdewa')).toBe(true)
   // expect(found('Namamu')).toBe(true)
 })
