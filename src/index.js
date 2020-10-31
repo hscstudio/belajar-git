bae7b9f6a0a52db5edebfeb00683268ef498730f
fmuiin14 <fmuiin14@gmail.com>
2020-10-31 16:33:27 +0700

    update fitur contributor fmuiin14

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 const contributors = [
-  { name: 'Hafid Mukhlasin', link: 'https://github.com/hscstudio' },
-  { name: 'Kungfu Koding', link: 'https://github.com/kungfukoding' },
-  { name: 'easyphpframework', link: 'https://github.com/easyphpframework' },
-  { name: 'Tio Andri Prasetyo', link: 'https://github.com/tioandri91' },
-  { name: 'Tesmo', link: 'https://github.com/tesmoinc/belajar-git' },
-  { name: 'Sigdewa', link: 'https://github.com/sigdewa/belajar-git' }
-  
+  { name: "Hafid Mukhlasin", link: "https://github.com/hscstudio" },
+  { name: "Kungfu Koding", link: "https://github.com/kungfukoding" },
+  { name: "easyphpframework", link: "https://github.com/easyphpframework" },
+  { name: "Tio Andri Prasetyo", link: "https://github.com/tioandri91" },
+  { name: "Tesmo", link: "https://github.com/tesmoinc/belajar-git" },
+  { name: "Sigdewa", link: "https://github.com/sigdewa/belajar-git" },
+  { name: "Fathul Muiin", link: "https://github.com/fmuiin14/belajar-git" },
+
   // { name: 'namamu', link: 'link_githubmu' },
-]
+];
 
 const found = (contributor) => {
   const result = contributors.find((item) => {
-    return contributor === item.name
-  })
-  return result ? true : false
-}
+    return contributor === item.name;
+  });
+  return result ? true : false;
+};
 
-module.exports = found
+module.exports = found;
